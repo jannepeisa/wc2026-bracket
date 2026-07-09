9a0b022de7c8a2d3ceeafc9ef947cce15bff5736
Janne Peisa <jpeisa@gmail.com>
2026-07-09 15:00:02 +0000

    data: auto-refresh odds/results 2026-07-09T15:00:02Z

--- bracket_data.js
+++ bracket_data.js
@@ -28,7 +28,7 @@ window.BRACKET_DATA = {
    "name": "Morocco",
    "short": "Morocco",
    "flag": "🇲🇦",
-   "elo": 1901.1
+   "elo": 1901.8
   },
   "Germany": {
    "name": "Germany",
@@ -46,7 +46,7 @@ window.BRACKET_DATA = {
    "name": "France",
    "short": "France",
    "flag": "🇫🇷",
-   "elo": 2074.4
+   "elo": 2073.7
   },
   "Sweden": {
    "name": "Sweden",
@@ -58,7 +58,7 @@ window.BRACKET_DATA = {
    "name": "Belgium",
    "short": "Belgium",
    "flag": "🇧🇪",
-   "elo": 1912.7
+   "elo": 1913.2
   },
   "Senegal": {
    "name": "Senegal",
@@ -82,7 +82,7 @@ window.BRACKET_DATA = {
    "name": "Spain",
    "short": "Spain",
    "flag": "🇪🇸",
-   "elo": 2064.2
+   "elo": 2063.7
   },
   "Austria": {
    "name": "Austria",
@@ -124,7 +124,7 @@ window.BRACKET_DATA = {
    "name": "Norway",
    "short": "Norway",
    "flag": "🇳🇴",
-   "elo": 1837.3
+   "elo": 1837.4
   },
   "Mexico": {
    "name": "Mexico",
@@ -142,7 +142,7 @@ window.BRACKET_DATA = {
    "name": "England",
    "short": "England",
    "flag": "🏴",
-   "elo": 1938.7
+   "elo": 1938.6
   },
   "DR Congo": {
    "name": "DR Congo",
@@ -154,7 +154,7 @@ window.BRACKET_DATA = {
    "name": "Switzerland",
    "short": "Switzerland",
    "flag": "🇨🇭",
-   "elo": 1861.7
+   "elo": 1862.3
   },
   "Algeria": {
    "name": "Algeria",
@@ -190,7 +190,7 @@ window.BRACKET_DATA = {
    "name": "Argentina",
    "short": "Argentina",
    "flag": "🇦🇷",
-   "elo": 2005.9
+   "elo": 2005.3
   },
   "Cape Verde": {
    "name": "Cape Verde",
@@ -347,19 +347,19 @@ window.BRACKET_DATA = {
    "date": "2026-07-07"
   },
   "France|Morocco": {
-   "p_home_adv": 0.7305,
+   "p_home_adv": 0.7289,
    "date": "2026-07-09"
   },
   "Norway|England": {
-   "p_home_adv": 0.3582,
+   "p_home_adv": 0.3584,
    "date": "2026-07-11"
   },
   "Spain|Belgium": {
-   "p_home_adv": 0.7052,
+   "p_home_adv": 0.7041,
    "date": "2026-07-10"
   },
   "Argentina|Switzerland": {
-   "p_home_adv": 0.6964,
+   "p_home_adv": 0.695,
    "date": "2026-07-12"
   }
  },
