c50d36f44885c0a1165571b9a2b6990ea5ddfa99
Janne Peisa <jpeisa@gmail.com>
2026-07-16 06:00:02 +0000

    data: auto-refresh odds/results 2026-07-16T06:00:02Z

--- bracket_data.js
+++ bracket_data.js
@@ -46,7 +46,7 @@ window.BRACKET_DATA = {
    "name": "France",
    "short": "France",
    "flag": "🇫🇷",
-   "elo": 2070.3
+   "elo": 2070.7
   },
   "Sweden": {
    "name": "Sweden",
@@ -82,7 +82,7 @@ window.BRACKET_DATA = {
    "name": "Spain",
    "short": "Spain",
    "flag": "🇪🇸",
-   "elo": 2035.7
+   "elo": 2036.7
   },
   "Austria": {
    "name": "Austria",
@@ -142,7 +142,7 @@ window.BRACKET_DATA = {
    "name": "England",
    "short": "England",
    "flag": "🏴",
-   "elo": 1995.3
+   "elo": 1994.9
   },
   "DR Congo": {
    "name": "DR Congo",
@@ -190,7 +190,7 @@ window.BRACKET_DATA = {
    "name": "Argentina",
    "short": "Argentina",
    "flag": "🇦🇷",
-   "elo": 1987.9
+   "elo": 1986.9
   },
   "Cape Verde": {
    "name": "Cape Verde",
@@ -371,11 +371,11 @@ window.BRACKET_DATA = {
    "date": "2026-07-15"
   },
   "France|England": {
-   "p_home_adv": 0.6063,
+   "p_home_adv": 0.6075,
    "date": "2026-07-18"
   },
   "Spain|Argentina": {
-   "p_home_adv": 0.5684,
+   "p_home_adv": 0.5713,
    "date": "2026-07-19"
   }
  },
